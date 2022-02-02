1ea2f2b55bc9643452f0916e84f259536d6ee1e5
David <David>
2022-02-02 22:29:35 +0100

    blckchain

--- main.py
+++ main.py
@@ -3,16 +3,29 @@ import time
 import json
 
 class Block:
-    def __init__(self, index, transactions, timestamp):
+    def __init__(self, index, transactions, timestamp, previous_hash):
 
         self.index = index
         self.transactions = transactions
         self.timestamp = timestamp
+        self.previous_hash = previous_hash
     
-    def compute_has(self):
+    def compute_hash(self):
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
 
-block = Block(1, "Hey como mola esto", time.time_ns())
 
-print(block.compute_has())
+class BlockChain():
+    def __init__(self):
+        self.chain = []
+        self.create_genesis()
+
+    def create_genesis(self):
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    def return_last(self):
+        return self.chain[-1]
+
+        
