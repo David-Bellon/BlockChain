cd1bd2f54fcda2c902f28d437e6f090907bc8a94
David <David>
2022-02-02 22:21:14 +0100

    Block

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from hashlib import sha256
+import time
 import json
 
 class Block:
@@ -8,6 +9,10 @@ class Block:
         self.transactions = transactions
         self.timestamp = timestamp
     
-    def compute_has(block):
-        block_string = json.dump(block.__dict__, sort_keys=True)
+    def compute_has(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
+
+block = Block(1, "Hey como mola esto", time.time_ns())
+
+print(block.compute_has())
