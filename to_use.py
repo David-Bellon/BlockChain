4393d3c1866cd8817837e4f1c718c48438903202
Carla <Carla2002.av@gmail.com>
2022-05-23 18:00:52 +0200

    actualizar touse

--- to_use.py
+++ to_use.py
@@ -42,28 +42,41 @@ class BlockChain():
             self.adress = adress
             self.notes = "Adress Created"
 
+    class EventNodeCreated():
+        def __init__(self, adress, ip) -> None:
+            self.adress = adress
+            self.ip = ip
+
+    class EventVote():
+        pass
+
     difficulty = 3
-    users = []
     
-    nodes = {
 
-    }
-
+    
     def __init__(self):
 
         self.unconfirmed_transactions = []
         self.chain = []
         self.create_genesis()
-
+        
+    
     def create_genesis(self):
+        self.nodes = {
+            }
+        self.users = []
         genesis_block = Block(0, [], time.time(), "0")
         genesis_block.hash = genesis_block.compute_hash()
         genesis_block.mineDate = time.time()
         self.add_node("0x0", get_ip())
+        self.add_user(User("0x0"))
         self.chain.append(genesis_block)
 
     def last_block(self):
         return self.chain[-1]
+
+    def add_node_transactin(self, adress, ip):
+        self.add_new_transaction(self.EventNodeCreated(adress, ip))
 
     def add_node(self, adress, ip):
         self.nodes[adress] = ip
@@ -75,10 +88,11 @@ class BlockChain():
             df = pd.DataFrame({"ip_nodes": [ip]})
             df.to_csv(url, index=False)
 
-    def add_user(self, user):
-        #create a transaction to store the user created
+
+    def add_user_transaction(self, user):
         self.add_new_transaction(self.EventUserCreated(user.adress))
 
+    def add_user(self, user):
         sem.acquire()
         self.users.append(user)
         sem.release()
@@ -125,7 +139,17 @@ class BlockChain():
         new_block = Block(index=last_block.index + 1, transactions=self.unconfirmed_transactions, timestamp=time.time(), previous_hash=last_block.hash)
         proof = self.proof_of_work(new_block)
         self.add_block(new_block, proof)
+        self.compute_transactions(new_block.transactions)
         self.unconfirmed_transactions = []
         print("Block Mined")
         sem.release()
         return new_block.index
+
+    def compute_transactions(self, transactions):
+        for i in transactions:
+            if i == self.EventUserCreated:
+                self.add_user(i.adress)
+            elif i == self.EventNodeCreated:
+                self.add_node(i.adress, i.ip)
+            else:
+                return False
